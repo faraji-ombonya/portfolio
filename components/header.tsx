c453f3b99b96e41bba85842b6c52e7d619d3a63e
Faraji Ombonya <farajiombonya@gmail.com>
2025-04-28 21:25:26 +0300

    feat: add toggle light scheme icon

--- components/header.tsx
+++ components/header.tsx
@@ -244,7 +244,20 @@ export default function Header() {
                         className="group rounded-full bg-white/90 px-3 py-2 shadow-lg ring-1 shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20  "
                       >
                         <div className="justify-center items-center">
-                          {/* <ion-icon name="sunny"></ion-icon> */}
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="size-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+                            />
+                          </svg>
                         </div>
                       </button>
                     </div>

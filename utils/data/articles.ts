5a6c88d7f8fe03f4c0095af3b86a5c8740b976c9
Faraji Ombonya <farajiombonya@gmail.com>
2025-11-28 16:18:55 +0300

    rewrite training word2vec as markdown

--- utils/data/articles.ts
+++ utils/data/articles.ts
@@ -1,16 +1,3 @@
-import { Article000001 } from "./articles/000001";
-import { Article000002 } from "./articles/000002";
 import { Article000003 } from "./articles/000003";
-import { Article000004 } from "./articles/000004";
 
-export const TITLE = "Writing on software engineering and the tech industry.";
-
-export const DESCRIPTION =
-  "My featured long-form thoughts on software engineering and related topics.";
-
-export const ARTICLES = [
-  Article000004,
-  Article000003,
-  Article000002,
-  Article000001,
-];
+export const ARTICLES = [Article000003];
